5b5e5dfdaac21001e06f1106cb51eb51b5a42f0f
Christian Rybarczyk <christian.j.rybarczyk@gmail.com>
2019-08-08 16:04:49 -0400

    Added displayProductDetails method

--- app/src/main/java/com/lambdaschool/android/organization/model/Product.kt
+++ app/src/main/java/com/lambdaschool/android/organization/model/Product.kt
@@ -1,7 +1,10 @@
 package com.lambdaschool.android.organization.model
 
-data class Product(val productName: String = "Hockey Puck",
-              val productDescription: String = "Rubber puck",
-              val productPrice: Double = 3.0,
-              val productTax: Double = productPrice * 0.085,
-              val productDiscount: Double = .10)
+import java.util.*
+
+data class Product(val orderDate: Date = Date(),
+                   val productName: String = "Hockey Puck",
+                   val productDescription: String = "Rubber puck",
+                   val productPrice: Double = 3.0,
+                   val productTax: Double = productPrice * 0.085,
+                   val productDiscount: Double = .10)
